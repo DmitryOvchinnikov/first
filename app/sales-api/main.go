dbe52cf76aeb143a100c74ff183b76079bf18b3f
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2022-09-25 14:00:30 +0300

    Config, debug, shutdown added to run.

--- app/sales-api/main.go
+++ app/sales-api/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"expvar"
 	"fmt"
+	"net/http"
+	_ "net/http/pprof" // Register the pprof handlers
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/ardanlabs/conf/v3"
@@ -11,6 +16,13 @@ import (
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
 )
+
+/*
+Need to figure out timeouts for http service.
+You might want to reset your DB_HOST env var during test tear down.
+Service should start even without a DB running yet.
+symbols in profiles: https://github.com/golang/go/issues/23376 / https://github.com/google/pprof/pull/366
+*/
 
 // build is the git version of this program.
 // It is set using build flags in the makefile.
@@ -40,7 +52,7 @@ func run(log *zap.SugaredLogger) error {
 		Configuration
 	*/
 
-	cfg := struct {
+	var cfg = struct {
 		conf.Version
 		Web struct {
 			ReadTimeout     time.Duration `conf:"default:5s"`
@@ -81,6 +93,93 @@ func run(log *zap.SugaredLogger) error {
 	log.Infow("startup", "config", out)
 
 	expvar.NewString("build").Set(build)
+
+	/*
+		Start Debug Service
+	*/
+
+	// /debug/pprof - Added to the default mux by importing the net/http/pprof package.
+	// /debug/vars - Added to the default mux by importing the expvar package.
+
+	// Not concerned with shutting this down when the application is shutdown.
+	log.Infow("startup", "status", "debug v1 router started", "host", cfg.Web.DebugHost)
+
+	// The Debug function returns a mux to listen and serve on for all the debug
+	// related endpoints. This includes the standard library endpoints.
+
+	// Construct the mux for the debug calls.
+	//debugMux := handlers.DebugMux(build, log, db)
+
+	// Start the service listening for debug requests.
+	// Not concerned with shutting this down with load shedding.
+	go func() {
+		if err := http.ListenAndServe(cfg.Web.DebugHost, http.DefaultServeMux); err != nil {
+			log.Errorw("shutdown", "status", "debug v1 router closed", "host", cfg.Web.DebugHost, "ERROR", err)
+		}
+	}()
+
+	/*
+		Start API Service
+	*/
+
+	log.Infow("startup", "status", "initializing API support")
+
+	// Make a channel to listen for an interrupt or terminate signal from the OS.
+	// Use a buffered channel because the signal package requires it.
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	//// Construct the mux for the API calls.
+	//apiMux := handlers.APIMux(handlers.APIMuxConfig{
+	//	Shutdown: shutdown,
+	//	Log:      log,
+	//	Auth:     auth,
+	//	DB:       db,
+	//})
+
+	// Construct a server to service the requests against the mux.
+	api := http.Server{
+		Addr: cfg.Web.APIHost,
+		//Handler:      apiMux,
+		ReadTimeout:  cfg.Web.ReadTimeout,
+		WriteTimeout: cfg.Web.WriteTimeout,
+		IdleTimeout:  cfg.Web.IdleTimeout,
+		ErrorLog:     zap.NewStdLog(log.Desugar()),
+	}
+
+	// Make a channel to listen for errors coming from the listener. Use a
+	// buffered channel so the goroutine can exit if we don't collect this error.
+	serverErrors := make(chan error, 1)
+
+	// Start the service listening for api requests.
+	go func() {
+		log.Infow("startup", "status", "api router started", "host", api.Addr)
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	/*
+		Shutdown
+	*/
+
+	// Blocking main and waiting for shutdown.
+	select {
+	case err := <-serverErrors:
+		return fmt.Errorf("server error: %w", err)
+
+	case sig := <-shutdown:
+		log.Infow("shutdown", "status", "shutdown started", "signal", sig)
+		defer log.Infow("shutdown", "status", "shutdown complete", "signal", sig)
+
+		// Give outstanding requests a deadline for completion.
+		ctx, cancel := context.WithTimeout(context.Background(), cfg.Web.ShutdownTimeout)
+		defer cancel()
+
+		// Asking listener to shutdown and shed load.
+		if err := api.Shutdown(ctx); err != nil {
+			api.Close()
+			return fmt.Errorf("could not stop server gracefully: %w", err)
+		}
+	}
 
 	return nil
 }
