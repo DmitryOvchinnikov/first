0853b1a85cb84b6bdd86884c5b79b7dde7a7090c
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2022-12-24 07:06:43 +0300

    5.5 Error Handling

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -6,8 +6,11 @@ import (
 	"net/http"
 	"os"
 	"syscall"
+	"time"
 
 	"github.com/dimfeld/httptreemux/v5"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel/trace"
 )
 
 // A Handler is a type that handles an http request within our own little mini
@@ -20,8 +23,8 @@ type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) e
 type App struct {
 	mux      *httptreemux.ContextMux
 	shutdown chan os.Signal
-	//otmux    http.Handler
-	//mw       []Middleware
+	otmux    http.Handler
+	mw       []Middleware
 }
 
 // NewApp creates an App value that handle a set of routes for the application.
@@ -39,8 +42,8 @@ func NewApp(shutdown chan os.Signal, mw ...Middleware) *App {
 	return &App{
 		mux:      mux,
 		shutdown: shutdown,
-		//otmux:    otelhttp.NewHandler(mux, "request"),
-		//mw:       mw,
+		otmux:    otelhttp.NewHandler(mux, "request"),
+		mw:       mw,
 	}
 }
 
@@ -54,19 +57,19 @@ func (a *App) SignalShutdown() {
 // all http traffic and allows the opentelemetry mux to run first to handle
 // tracing. The opentelemetry mux then calls the application mux to handle
 // application traffic. This was setup on line 44 in the NewApp function.
-//func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-//	a.otmux.ServeHTTP(w, r)
-//}
+func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	a.otmux.ServeHTTP(w, r)
+}
 
 // Handle sets a handler function for a given HTTP method and path pair
 // to the application server mux.
 func (a *App) Handle(method string, group string, path string, handler Handler, mw ...Middleware) {
 
 	// First wrap handler specific middleware around this handler.
-	//handler = wrapMiddleware(mw, handler)
+	handler = wrapMiddleware(mw, handler)
 
 	// Add the application's general middleware to the handler chain.
-	//handler = wrapMiddleware(a.mw, handler)
+	handler = wrapMiddleware(a.mw, handler)
 
 	// The function to execute for each request.
 	h := func(w http.ResponseWriter, r *http.Request) {
@@ -76,15 +79,15 @@ func (a *App) Handle(method string, group string, path string, handler Handler, 
 		ctx := r.Context()
 
 		// Capture the parent request span from the context.
-		//span := trace.SpanFromContext(ctx)
+		span := trace.SpanFromContext(ctx)
 
 		// Set the context with the required values to
 		// process the request.
-		//v := Values{
-		//	TraceID: span.SpanContext().TraceID().String(),
-		//	Now:     time.Now(),
-		//}
-		//ctx = context.WithValue(ctx, key, &v)
+		v := Values{
+			TraceID: span.SpanContext().TraceID().String(),
+			Now:     time.Now(),
+		}
+		ctx = context.WithValue(ctx, key, &v)
 
 		// Call the wrapped handler functions.
 		if err := handler(ctx, w, r); err != nil {
